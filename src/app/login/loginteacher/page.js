d48dc061e0a00a037807034e14f47d38c7ca65ab
Darkiee <amazdeepanshu@gmail.com>
2026-02-16 15:21:49 +0530

    page.js

--- src/app/login/loginteacher/page.js
+++ src/app/login/loginteacher/page.js
@@ -2,6 +2,7 @@ const Loginteacher =()=>{
     return(
         <div>
             <h1 className="heading">login page of teachers</h1>
+               <h1 className="heading">login page of Students</h1>
         </div>
     )
 }
