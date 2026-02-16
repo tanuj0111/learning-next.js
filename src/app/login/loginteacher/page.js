b8a0cc2f14f7a6b76871c1c18b7cf065a4802116
Chandan0Singh <chandalien@5025@gmail.com>
2026-02-16 15:23:13 +0530

    main change

--- src/app/login/loginteacher/page.js
+++ src/app/login/loginteacher/page.js
@@ -2,6 +2,7 @@ const Loginteacher =()=>{
     return(
         <div>
             <h1 className="heading">login page of teachter</h1>
+            <h2 className="heading">there is a  teachter</h2>
         </div>
     )
 }
